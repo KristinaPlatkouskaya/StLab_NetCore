8155d0816eea81577405c3304e554d22743210f2
Kristina_Platkovskaya <kristina.platkovskaya@mail.ru>
2018-08-09 14:38:05 +0300

    Create task1

---
+++ Task1/Models/SumResultModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task1.Models
+{
+    public class SumResultModel
+    {
+        [Required(ErrorMessage = "Параметр должен быть определен")]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Параметр должен быть положительным")]
+        public int? A { get; set; }
+
+        [Required(ErrorMessage = "Параметр должен быть определен")]
+        [Range(Int32.MinValue, -1, ErrorMessage = "Параметр должен быть отрицательным")]
+        public int? B { get; set; }
+
+        public int Sum { get; set; }
+    }
+}
+
