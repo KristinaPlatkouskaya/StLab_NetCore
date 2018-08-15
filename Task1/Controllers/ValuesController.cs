eb4ace1a6fd6df01bd99d9e401e5631daea69f95
Kristina_Platkovskaya <kristina.platkovskaya@mail.ru>
2018-08-15 15:55:01 +0300

    Add lost files

---
+++ Task1/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task1.Models;
+
+namespace task1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get(ValueModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return Ok(new SumResultModel { A = (int)model.A, B = (int)model.B, Sum = (int)(model.A + model.B) });
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+
+        }
+    }
+}
